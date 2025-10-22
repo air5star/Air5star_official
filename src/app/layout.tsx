24a6a2c5d80cfa84ef8ef8e9ba67eec48bf34ae2
air5star <info.air5star@gmail.com>
2025-10-23 00:24:34 +0530

    uu

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,1 +1,21 @@
- 
+import './globals.css'
+import Script from 'next/script'
+import React from 'react'
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body>
+        {children}
+        <Script
+          src="//code.tidio.co/wzdpnxu7gfb2wop0hofuw4acrzmjscsw.js"
+          strategy="afterInteractive"
+        />
+      </body>
+    </html>
+  )
+}
